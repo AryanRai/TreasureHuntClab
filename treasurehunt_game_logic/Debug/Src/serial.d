68bc8cc34bf0fc83eb1e3346269a61d0e839b63b
SHughes93 <59671593+SHughes93@users.noreply.github.com>
2025-05-18 12:40:59 +1000

    game_logic
    Game logic added. Piecing together

---
+++ treasurehunt_game_logic/Debug/Src/serial.d
@@ -0,0 +1,19 @@
+Src/serial.o: ../Src/serial.c \
+ C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/stm32f303xc.h \
+ C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/core_cm4.h \
+ C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/cmsis_version.h \
+ C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/cmsis_compiler.h \
+ C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/cmsis_gcc.h \
+ C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/mpu_armv7.h \
+ C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/system_stm32f3xx.h \
+ ../Inc/gamestate.h ../Inc/serial.h ../Inc/gamestate.h
+C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/stm32f303xc.h:
+C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/core_cm4.h:
+C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/cmsis_version.h:
+C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/cmsis_compiler.h:
+C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/cmsis_gcc.h:
+C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/mpu_armv7.h:
+C:/Users/163910/OneDrive\ -\ UTS/Documents/GitHub/Send\ files\ i\ am\ mucking\ aorund\ with/TreasureHunt/stm32f303-definitions/Core/Inc/system_stm32f3xx.h:
+../Inc/gamestate.h:
+../Inc/serial.h:
+../Inc/gamestate.h:
